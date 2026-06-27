436c859067e83bbb264ab1c9aa08d8d20d297609
an-thony350 <afob2@hotmail.com>
2026-06-27 16:42:40 +0100

    [update] added self checking to dataclasses and some small bugfixes

--- golden/contracts.py
+++ golden/contracts.py
@@ -20,14 +20,20 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Optional
 
-class Op(Enum):
+LOCATE_BITS = 16
+ORDER_REF_BITS = 64
+PRICE_BITS = 32
+SHARES_BITS = 32
+TIMESTAMP_BITS = 48
+
+class Op(str, Enum):
     ADD = "ADD"
     EXECUTE = "EXECUTE"
     CANCEL = "CANCEL"
     DELETE = "DELETE"
     REPLACE = "REPLACE"
 
-class Side(Enum):
+class Side(str, Enum):
     BUY = "BUY"
     SELL = "SELL"
     UNKNOWN = "UNKNOWN"
@@ -46,21 +52,59 @@ class NormalisedEvent:
 
     new_order_ref: Optional[int] = None
 
-    shares: Optional[int] = None
-
     timestamp_ns: Optional[int] = None
+
+    def __post_init__(self) -> None:
+        if self.op == Op.ADD:
+            assert self.side in (Side.BUY, Side.SELL)
+            assert self.price is not None
+            assert self.shares is not None
+            assert self.new_order_ref is None
+        
+        elif self.op == Op.EXECUTE:
+            assert self.side == Side.UNKNOWN
+            assert self.price is None
+            assert self.shares is not None
+            assert self.new_order_ref is None
+
+        elif self.op == Op.CANCEL:
+            assert self.side == Side.UNKNOWN
+            assert self.price is None
+            assert self.shares is not None
+            assert self.new_order_ref is None
+        
+        elif self.op == Op.DELETE:
+            assert self.side == Side.UNKNOWN
+            assert self.price is None
+            assert self.shares is None
+            assert self.new_order_ref is None
+
+        elif self.op == Op.REPLACE:
+            assert self.side == Side.UNKNOWN
+            assert self.price is not None
+            assert self.shares is not None
+            assert self.new_order_ref is not None
 
 @dataclass(frozen=True)
 class Bbo:
     bid_price: Optional[int]
-    bid_size: int
+    bid_size: Optional [int]
     ask_price: Optional[int]
     ask_size: Optional[int]
+
+    def __post_init__(self) -> None:
+        assert (self.bid_price is None) == (self.bid_size is None)
+        assert (self.ask_price is None) == (self.ask_size is None)
+
+@dataclass(frozen=True)
+class Level:
+    shares: int
+    order_count: int
 
 @dataclass(frozen=True)
 class BookState:
     msg_index: int
     bbo: Bbo
 
-    bid_levels: dict[int, int]
-    ask_levels: dict[int, int]
+    bid_levels: dict[int, Level]
+    ask_levels: dict[int, Level]
