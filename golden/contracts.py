b25a0a1845e1d5ab1f36c83eaba47e6dd571cb27
an-thony350 <afob2@hotmail.com>
2026-06-27 16:12:45 +0100

    [update] defining dataclasses

--- golden/contracts.py
+++ golden/contracts.py
@@ -15,3 +15,52 @@ Need a couple of things
 - Bbo: top of book representation
 - BookState: a class that represents the state of the book at a given time
 """
+
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
+
+class Op(Enum):
+    ADD = "ADD"
+    EXECUTE = "EXECUTE"
+    CANCEL = "CANCEL"
+    DELETE = "DELETE"
+    REPLACE = "REPLACE"
+
+class Side(Enum):
+    BUY = "BUY"
+    SELL = "SELL"
+    UNKNOWN = "UNKNOWN"
+
+@dataclass(frozen=True) # frozen true to prevent changes to state after creation
+class NormalisedEvent:
+    op: Op
+    locate: int
+    side: Side
+    order_ref: int
+    msg_index: int
+
+    price: Optional[int] = None
+
+    shares: Optional[int] = None
+
+    new_order_ref: Optional[int] = None
+
+    shares: Optional[int] = None
+
+    timestamp_ns: Optional[int] = None
+
+@dataclass(frozen=True)
+class Bbo:
+    bid_price: Optional[int]
+    bid_size: int
+    ask_price: Optional[int]
+    ask_size: Optional[int]
+
+@dataclass(frozen=True)
+class BookState:
+    msg_index: int
+    bbo: Bbo
+
+    bid_levels: dict[int, int]
+    ask_levels: dict[int, int]
