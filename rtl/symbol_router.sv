3b6f64a802a4adf377932b508cb0963450a0c3a4
derza-essien <d.erzaessien@outlook.com>
2026-06-30 13:51:51 +0100

    [update] symbol router skeleton w/o stocks

--- rtl/symbol_router.sv
+++ rtl/symbol_router.sv
@@ -48,38 +48,59 @@ module symbol_router#(
 
     // outputs to order book
     output  o_data_t                rdata_o,
-    output  logic                   valid_o,
     output  logic   [PRICE_W-1:0]   base_price_o
+
+    // stock outputs to order book - names will change
+    output  logic                   valid_stock0_o,
+    output  logic                   valid_stock1_o,
+    output  logic                   valid_stock2_o,
+    output  logic                   valid_stock3_o
+
 );
 
-logic   [PRICE_W-1:0]   bp_list [31:0];
-logic   [31:0]          bp_idx
+logic   [PRICE_W-1:0]   bp_list [4];
 
-always_comb begin
-    if(valid_i) begin
-        case(rdata_i.stock_locate)
-        // select stocks (havent decided yet)
-        // will have form bp_idx = fixed value for that stock e.g. appl = 0, msft = 1...
+// combination I/O assignment
 
-        default: ;
-        endcase
-    end
-end
+assign rdata_o.message_type = rdata_i.message_type;
+assign rdata_o.orn          = rdata_i.orn;
+assign rdata_o.updated_orn  = rdata_i.updated_orn;
+assign rdata_o.side         = rdata_i.side;
+assign rdata_o.shares       = rdata_i.shares;
+assign rdata_o.price        = rdata_i.price;
 
 always_ff@(posedge clk) begin
     if(!rst_n) begin
-        // clear state probably
+        valid_stock0_o  <=  1'b0;
+        valid_stock1_o  <=  1'b0;
+        valid_stock2_o  <=  1'b0;
+        valid_stock3_o  <=  1'b0;
     end
     else begin
-        if(ready_i) begin
-            base_price_o            <=  bp_list[bp_idx];
-            rdata_i.message_type    <=  rdata_o.message_type;
-            rdata_i.orn             <=  rdata_o.orn;
-            rdata_i.price           <=  rdata_o.price;
-            rdata_i.shares          <=  rdata_o.shares;
-            rdata_i.side            <=  rdata_o.side;
-            rdata_i.updated_orn     <=  rdata_o.updated_orn;
-            valid_o                 <=  1'b1;
+        valid_stock0_o  <=  1'b0;
+        valid_stock1_o  <=  1'b0;
+        valid_stock2_o  <=  1'b0;
+        valid_stock3_o  <=  1'b0;
+        if(valid_i) begin
+            case(rdata_i.stock_locate) // actual values not added yet as stocks undecided
+                16'd0:  begin
+                    valid_stock0_o  <=  1'b1;
+                    base_price_o    <=  bp_list[0];
+                end
+                16'd1:  begin
+                    valid_stock1_o  <=  1'b1;
+                    base_price_o    <=  bp_list[1];
+                end
+                16'd2:  begin
+                    valid_stock2_o  <=  1'b1;
+                    base_price_o    <=  bp_list[2];
+                end
+                16'd3:  begin
+                    valid_stock3_o  <=  1'b1;
+                    base_price_o    <=  bp_list[3];
+                end
+                default:    ;
+            endcase
         end
     end
 end
