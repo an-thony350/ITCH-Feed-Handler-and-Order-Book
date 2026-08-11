1fbc3aec38b448584ef9275d4c7d32421b056d81
derza-essien <d.erzaessien@outlook.com>
2026-08-11 16:58:25 +0100

    [update] idx_search order book block

--- rtl/ob_idx_search.sv
+++ rtl/ob_idx_search.sv
@@ -2,20 +2,159 @@ import hdl_header::*
 
 module ob_idx_search(
     // Control signals
-    input logic             clk,
-    input logic             rst_n,
-    input logic             stall,
+    input logic                 clk,
+    input logic                 rst_n,
+    input logic                 stall,
 
     // Instruction Data I/O
-    input o_data_t          rdata_i,
-    input logic             is_add_i,
-    input logic             is_delete_i,
-    input logic             is_reduce_i,
-    input logic             is_replace_i,
+    input logic                 stage_valid_i,
+    input o_data_t              latched_rdata_i,
+    input logic [PRICE_W-1:0]   latched_base_price_i,
+    input logic                 latched_is_add_i,
+    input logic                 latched_is_reduce_i,
+    input logic                 latched_is_replace_i,
+    input logic                 latched_is_delete_i,
+
+    output logic                stage_valid_o,
+    output o_data_t             latched_rdata_o,
+    output logic [PRICE_W-1:0]  latched_base_price_o,
+    output logic                latched_is_add_o,
+    output logic                latched_is_reduce_o,
+    output logic                latched_is_replace_o,
+    output logic                latched_is_delete_o,
 
     // Computed DataPath I/O
-    input logic [1:0]       comb_slot_idx_i,
-    input logic [1:0]       rep_comb_slot_idx_i
+    input logic [BBO_W-1:0]     latched_event_price_idx_i,
+    input logic                 latched_cam_hit_i,
+    input logic [5:0]           latched_cam_match_idx_i,
+    input logic                 latched_cam_is_full_i,
+    input logic [5:0]           latched_cam_free_idx_i,
+
+    output logic [BBO_W-1:0]    latched_event_price_idx_o,
+    output logic                latched_cam_hit_o,
+    output logic [5:0]          latched_cam_match_idx_o,
+    output logic [5:0]          latched_cam_free_idx_o,
+    output logic [1:0]          latched_slot_idx_o,
+    output logic [1:0]          latched_rep_slot_idx_o,
+
+    // External Memory I/O
+    input order_entry_t [2:0]   read_bucket,
+    input order_entry_t [2:0]   rep_read_bucket
 );
+
+// Internal Registers
+
+// comb 3-way associative hash regs
+logic [2:0] hash_match;
+logic [2:0] free_slot;
+logic [2:0] rep_hash_match;
+logic [2:0] rep_free_slot;
+
+// idx search regs
+logic [1:0] comb_slot_idx;
+logic [1:0] rep_comb_slot_idx;
+
+// combinational logic determing hash matches and free slots
+always_comb begin
+    // Default Assignments
+    hash_match      =   '0;
+    free_slot       =   '0;
+    rep_hash_match  =   '0;
+    rep_free_slot   =   '0;
+
+    hash_match[0]        =   (read_bucket[0].valid && read_bucket[0].orn == latched_rdata.orn && ~read_bucket[0].tombstone);
+    hash_match[1]        =   (read_bucket[1].valid && read_bucket[1].orn == latched_rdata.orn && ~read_bucket[1].tombstone);
+    hash_match[2]        =   (read_bucket[2].valid && read_bucket[2].orn == latched_rdata.orn && ~read_bucket[2].tombstone);
+
+    free_slot[0]         = (!read_bucket[0].valid || read_bucket[0].tombstone);
+    free_slot[1]         = (!read_bucket[1].valid || read_bucket[1].tombstone);
+    free_slot[2]         = (!read_bucket[2].valid || read_bucket[2].tombstone);
+
+    rep_hash_match[0]    =   (rep_read_bucket[0].valid && rep_read_bucket[0].orn == latched_rdata.orn && ~rep_read_bucket[0].tombstone);
+    rep_hash_match[1]    =   (rep_read_bucket[1].valid && rep_read_bucket[1].orn == latched_rdata.orn && ~rep_read_bucket[1].tombstone);
+    rep_hash_match[2]    =   (rep_read_bucket[2].valid && rep_read_bucket[2].orn == latched_rdata.orn && ~rep_read_bucket[2].tombstone);
+
+    rep_free_slot[0]    = (!rep_read_bucket[0].valid || rep_read_bucket[0].tombstone);
+    rep_free_slot[1]    = (!rep_read_bucket[1].valid || rep_read_bucket[1].tombstone);
+    rep_free_slot[2]    = (!rep_read_bucket[2].valid || rep_read_bucket[2].tombstone);
+end
+
+
+// combinational logic determining if forwarding is required
+
+always_comb begin
+    comb_slot_idx       =   '0;
+    rep_comb_slot_idx   =   '0;
+
+    if(latched_is_add_i) begin
+        if(free_slot != 3'b000) begin
+            // some type of logic specifying no forward instuction?
+            if      (free_slot[0]) comb_slot_idx = 2'd0;
+            else if (free_slot[1]) comb_slot_idx = 2'd1;
+            else                   comb_slot_idx = 2'd2;
+        end
+    end
+    else if(latched_is_delete_i || latched_is_reduce_i || latched_is_replace_i) begin
+        if(hash_match != 3'b000) begin
+            if      (hash_match[0]) comb_slot_idx = 2'd0;
+            else if (hash_match[1]) comb_slot_idx = 2'd1;
+            else                    comb_slot_idx = 2'd2;
+
+            if(latched_is_replace_i) begin
+                if(rep_free_slot != 3'b000) begin
+                    if      (rep_free_slot[0]) rep_comb_slot_idx = 2'd0;
+                    else if (rep_free_slot[1]) rep_comb_slot_idx = 2'd1;
+                    else                       rep_comb_slot_idx = 2'd2;
+                end
+                else if(latched_cam_is_full) begin
+                    // forwarding logic
+                end
+            end
+        end
+    end
+end
+
+// seq logic
+always_ff @(posedge clk) begin
+    if(!rst_n) begin
+        stage_valid_o               <=  '0;
+        latched_rdata_o             <=  '0;
+        latched_base_price_o        <=  '0;
+        latched_is_add_o            <=  1'b0;
+        latched_is_reduce_o         <=  1'b0;
+        latched_is_replace_o        <=  1'b0;
+        latched_is_delete_o         <=  1'b0;
+
+        latched_event_price_idx_o   <=  '0;
+        latched_cam_hit_o           <=  1'b0;
+        latched_cam_match_idx_o     <=  '0;
+        latched_cam_free_idx_o      <=  '0;
+        latched_slot_idx_o          <=  '0;
+        latched_rep_slot_idx_o      <=  '0;
+    end
+    else if(!stall) begin
+        // deals with immediate return to FETCH_BBO state in old design
+        if(hash_match == 3'b000 && !latched_cam_hit_i) begin
+            stage_valid_o   <=  1'b0;
+        end
+        else begin
+            stage_valid_o   <=  stage_valid_i;
+        end
+
+        latched_rdata_o             <=  latched_rdata_i;
+        latched_base_price_o        <=  latched_base_price_i;
+        latched_is_add_o            <=  latched_is_add_i;
+        latched_is_reduce_o         <=  latched_is_reduce_i;
+        latched_is_replace_o        <=  latched_is_replace_i;
+        latched_is_delete_o         <=  latched_is_delete_i;
+
+        latched_event_price_idx_o   <=  latched_event_price_idx_i;
+        latched_cam_hit_o           <=  latched_cam_hit_i;
+        latched_cam_match_idx_o     <=  latched_cam_match_idx_i;
+        latched_cam_free_idx_o      <=  latched_cam_free_idx_i;
+        latched_slot_idx_o          <=  comb_slot_idx;
+        latched_rep_slot_idx_o      <=  rep_comb_slot_idx;
+    end
+end
 
 endmodule
